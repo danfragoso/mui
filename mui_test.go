abecf08281484622f685511b20aa747b42d28811
danfragoso <danilo.fragoso@gmail.com>
2020-08-02 18:48:13 -0300

    Move unit test to mui pacakge

--- mui_test.go
+++ mui_test.go
@@ -6,32 +6,6 @@ import (
 	"testing"
 )
 
-type UnitTest struct {
-	name            string
-	filename        string
-	description     string
-	testingFunction func(*UnitTest) error
-}
-
-func (test *UnitTest) Run() error {
-	return test.testingFunction(test)
-}
-
-func (test *UnitTest) Fail(err error, t *testing.T) {
-	fmt.Println("\u001b[41;1m FAIL \u001b[0m " + test.name)
-	fmt.Printf("\u001b[31mError:\u001b[0m " + err.Error() + "\n")
-	fmt.Println(test.description)
-
-	fmt.Print("\n")
-	t.Error(err)
-}
-
-func (test *UnitTest) Pass() {
-	fmt.Println("\u001b[42;1m PASS \u001b[0m " + test.name)
-	fmt.Println(test.description)
-	fmt.Println("")
-}
-
 func TestParser(t *testing.T) {
 	fmt.Print("Testing MUI parser...\n\n")
 	var parserTests []*UnitTest
